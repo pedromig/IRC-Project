c368211b87e8195b2cd1647caaf39cfed6d8fe1a
pedromig <pedromiguelrodrigues2000@gmail.com>
2019-12-17 13:30:35 +0000

    Adding Report

---
+++ cmake-build-debug/CMakeFiles/IRC_Project.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pedro/Documents/Universidade/2ºano/1ºSemestre/IRC/Project/IRC-Project/Client/client.c" "/home/pedro/Documents/Universidade/2ºano/1ºSemestre/IRC/Project/IRC-Project/cmake-build-debug/CMakeFiles/IRC_Project.dir/Client/client.c.o"
+  "/home/pedro/Documents/Universidade/2ºano/1ºSemestre/IRC/Project/IRC-Project/Functions/functions.c" "/home/pedro/Documents/Universidade/2ºano/1ºSemestre/IRC/Project/IRC-Project/cmake-build-debug/CMakeFiles/IRC_Project.dir/Functions/functions.c.o"
+  "/home/pedro/Documents/Universidade/2ºano/1ºSemestre/IRC/Project/IRC-Project/Proxy/ircproxy.c" "/home/pedro/Documents/Universidade/2ºano/1ºSemestre/IRC/Project/IRC-Project/cmake-build-debug/CMakeFiles/IRC_Project.dir/Proxy/ircproxy.c.o"
+  "/home/pedro/Documents/Universidade/2ºano/1ºSemestre/IRC/Project/IRC-Project/Server/server.c" "/home/pedro/Documents/Universidade/2ºano/1ºSemestre/IRC/Project/IRC-Project/cmake-build-debug/CMakeFiles/IRC_Project.dir/Server/server.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/IRC_Project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IRC_Project.dir/Client/client.c.o"
+  "CMakeFiles/IRC_Project.dir/Functions/functions.c.o"
+  "CMakeFiles/IRC_Project.dir/Proxy/ircproxy.c.o"
+  "CMakeFiles/IRC_Project.dir/Server/server.c.o"
+  "IRC_Project"
+  "IRC_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/IRC_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
